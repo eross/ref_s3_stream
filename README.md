1af7d1fa122b82b4d6c0e1b7d46009527346abbd
Eric Ross <eric.ross@@hp.com>
2021-02-20 15:37:54 -0800

    /s3download works correctly

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
 # Demonstration of Streaming from S3
-
+##Endpoints
 There are three endpoints:
 
-1. /memdownload -- brute force uses memory
-1. /download -- reads 100 bytes at a time.  Needs very little memory.
+1. /memdownload?image=file -- brute force uses memory
+1. /download?image=file -- reads 100 bytes at a time.  Needs very little memory.
+1. /s3download?image=file -- Downloads files from s3 bucket defined in
+   [application.properties](src/main/java/resources/application.properties)
 
-Other suggestions can be found at:
+Note that s3download is very poor performance.  It is using small buffer sizes and logging after each buffer.
+It is also urging the jvm to do a garbage collection after every buffer.  Needless to say, delete the logging and the gc while
+increasing the buffer size and performance should be much better.  Not that will logging the download the free memory does not
+change much.  This can download any file as long as the timeout for downloads is not exceeded.  Again see
+[application.properties](src/main/java/resources/application.properties) for the tuning parameter.
+## References
 
-[download a file from Spring boot rest service](https://stackoverflow.com/questions/35680932/download-a-file-from-spring-boot-rest-service)
+
+* [download a file from Spring boot rest service](https://stackoverflow.com/questions/35680932/download-a-file-from-spring-boot-rest-service)
+* [AWS S3 Java SDK Examples](https://docs.aws.amazon.com/sdk-for-java/latest/developer-guide/examples-s3-objects.html)
--- src/main/java/com/rossmbox/filedownload/rest/DownloadController.java
+++ src/main/java/com/rossmbox/filedownload/rest/DownloadController.java
@@ -2,6 +2,7 @@ package com.rossmbox.filedownload.rest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
@@ -15,18 +16,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.core.ResponseInputStream;
+import software.amazon.awssdk.core.internal.util.Mimetype;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
+import software.amazon.awssdk.services.s3.model.S3Object;
+import software.amazon.awssdk.services.s3.model.S3Response;
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 @Controller
 public class DownloadController {
+
+    @Value("${filedownload.s3bucket}")
+    private String s3Bucket;
+
+    @Value("${filedownload.s3region}")
+    private String s3Region;
+
+
     Logger logger = LoggerFactory.getLogger(DownloadController.class);
     private static final String EXTENSION = ".jpg";
     private static final String SERVER_LOCATION = "/server/images";
@@ -62,7 +77,7 @@ public class DownloadController {
         File file = new ClassPathResource(SERVER_LOCATION + File.separator + image).getFile();
         Path path = file.toPath();
         String mimeType = Files.probeContentType(path);
-        logger.info("mimeType for "+path+" is " + mimeType);
+        logger.info("mimeType for " + path + " is " + mimeType);
 
 
         response.setContentType(mimeType);
@@ -78,4 +93,48 @@ public class DownloadController {
         };
     }
 
+    @RequestMapping(path = "/s3download", method = RequestMethod.GET)
+    public StreamingResponseBody s3DownloadFile(HttpServletResponse response, @RequestParam("image") String image) throws IOException {
+        File file = new File(image);
+        Path path = file.toPath();
+        ProfileCredentialsProvider profileCredentialsProvider = ProfileCredentialsProvider.create();
+        S3Client s3Client = S3Client.builder()
+                .region(Region.of(s3Region))
+                .credentialsProvider(profileCredentialsProvider)
+                .build();
+        logger.info("Retrieving s3://" + s3Bucket + "/" + file.toPath());
+
+        logger.info(file.toPath().toString());
+        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                .bucket(s3Bucket)
+                .key(file.toPath().toString())
+                .build();
+
+        // This code for detecting mime type will download the whole file into local file space.  Bad idea.
+        //GetObjectResponse objectResponse = s3Client.getObject(getObjectRequest, file.toPath());
+        //String mimeType = objectResponse.contentType();
+
+        String mimeType = Mimetype.MIMETYPE_OCTET_STREAM;
+        logger.info("mimeType for " + path + " is " + mimeType);
+
+        ResponseInputStream<GetObjectResponse> s3InputStream = s3Client.getObject(getObjectRequest);
+
+
+        // This is where the data starts to transfer.
+        response.setContentType(mimeType);
+        response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=\"" + file.getName());
+
+        return outputStream -> {
+            final int BUFFER_SIZE=1000;
+            InputStream inputStream = new BufferedInputStream(s3InputStream, BUFFER_SIZE);
+            int bytesRead;
+            byte[] buffer = new byte[BUFFER_SIZE];
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                logger.info("Outputting: "+String.valueOf(bytesRead));
+                logger.info("Memory free: "+String.valueOf(Runtime.getRuntime().freeMemory()));
+                System.gc();  // Be aggressive on gc to maximize free memory at all times.  Not recommended for production code.
+                outputStream.write(buffer, 0, bytesRead);
+            }
+        };
+    }
 }
