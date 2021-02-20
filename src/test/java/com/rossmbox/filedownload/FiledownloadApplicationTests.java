a9e67c12557b9a156a229465e4709bcd9dc39051
Eric Ross <epr@rossmbox.com>
2021-02-19 20:02:51 -0800

    INIT

---
+++ src/test/java/com/rossmbox/filedownload/FiledownloadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rossmbox.filedownload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiledownloadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
